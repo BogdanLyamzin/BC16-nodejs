1bd68be7949f4ea7eb30ecdfa7afb202e23b5669
unknown <bogdan.lyamzin.d@gmail.com>
2022-11-20 22:31:15 +0200

    remove mongodb URi from lesson 5

--- lesson-5/mongodb-project/app.js
+++ lesson-5/mongodb-project/app.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-const DB_HOST = "mongodb+srv://Bogdan:pBje6ZbFRATcsTq@cluster0.oqxqf.mongodb.net/books_reader?retryWrites=true&w=majority"
-
 mongoose.connect(DB_HOST)
     .then(()=> console.log("Database connect"))
     .catch((error)=> console.log(error.message))
